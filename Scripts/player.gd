5f642660e2b188117bf0329cd1bcec6b3cc6ee0d
Ryan <ryanaapostolo@gmail.com>
2026-04-30 22:27:04 -0300

    adicionei as setinhas

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,8 +8,6 @@ func _physics_process(delta):
 	var direction = Vector2.ZERO
 	
 	# Movimento horizontal + vertical
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
