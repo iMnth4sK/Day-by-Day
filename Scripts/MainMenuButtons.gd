fce01ef55925ae98e6d4d385d24e58f0ced343e7
iMnth4sK <iankadujulia@gmail.com>
2026-05-04 02:22:12 -0300

    Adicionei tela de loading e um mapa basico pra testes

--- Scripts/MainMenuButtons.gd
+++ Scripts/MainMenuButtons.gd
@@ -3,12 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	pass # Replace with function body
 
 
 func _on_sair_pressed() -> void:
@@ -20,4 +15,4 @@ func _on_opções_pressed() -> void:
 
 
 func _on_novo_jogo_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/principal.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Loading.tscn")
---
+++ Scripts/loading.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var path = "res://Scenes/principal.tscn"
+var loaded = false
+
+@onready var bar = $CanvasLayer/ProgressBar
+
+func _ready():
+	var err = ResourceLoader.load_threaded_request(path)
+	
+	if err != OK:
+		print("Erro ao iniciar carregamento:", err)
+
+func _process(_delta):
+	if loaded:
+		return
+
+	var progress = []
+	var status = ResourceLoader.load_threaded_get_status(path, progress)
+
+	# Atualiza barra
+	if progress.size() > 0:
+		bar.value = progress[0] * 100
+
+	# Sucesso
+	if status == ResourceLoader.THREAD_LOAD_LOADED:
+		loaded = true
+		var scene = ResourceLoader.load_threaded_get(path)
+		get_tree().change_scene_to_packed(scene)
+
+	# ERRO (isso evita travar infinito)
+	elif status == ResourceLoader.THREAD_LOAD_FAILED:
+		print("Falha ao carregar a cena!")
